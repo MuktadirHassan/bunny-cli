1e83c6f0e58ca4c6696210d03bdf19c2e8954dba
MuktadirHassan <tamimmuktadir@gmail.com>
2025-01-18 04:34:30 +0600

    feat: implement UploadFile function for uploading files to BunnyCDN storage

---
+++ bunny/upload.go
@@ -0,0 +1,69 @@
+/*
+Upload a file to a storage zone based on the URL path. If the directory tree does not exist, it will be created automatically.
+Doc: https://docs.bunny.net/reference/put_-storagezonename-path-filename
+API: https://{region}.storage.bunnycdn.com/{STORAGE_ZONE_NAME}/{path}/{fileName}
+*/
+package bunny
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+// Note:
+// localPath is the path to the file on the local machine, and remotePath is the path to the file on the BunnyCDN storage zone.
+// remotePath will have the same directory structure as localPath
+// For example, if localPath is ./path/to/file.txt, then remotePath will be be /path/to/file.txt
+
+func UploadFile(localPath string, relativePath string) error {
+	// Validate env vars
+	STORAGE_REGION := os.Getenv("STORAGE_REGION") // Optional; default is "sg"
+	STORAGE_ZONE_NAME := os.Getenv("STORAGE_ZONE_NAME")
+	STORAGE_ACCESS_KEY := os.Getenv("STORAGE_ACCESS_KEY")
+	if STORAGE_ACCESS_KEY == "" || STORAGE_ZONE_NAME == "" {
+		return fmt.Errorf("env STORAGE_ACCESS_KEY or STORAGE_ZONE_NAME is not set")
+	}
+
+	if STORAGE_REGION == "" {
+		STORAGE_REGION = "sg"
+	}
+
+	uploadPath := fmt.Sprintf("https://%s.storage.bunnycdn.com/%s/%s", STORAGE_REGION, STORAGE_ZONE_NAME, filepath.ToSlash(relativePath))
+	file, err := os.Open(localPath)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s: %v", localPath, err)
+	}
+	defer file.Close()
+
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("failed to read file %s: %v", localPath, err)
+	}
+
+	req, err := http.NewRequest("PUT", uploadPath, bytes.NewReader(fileData))
+	if err != nil {
+		return fmt.Errorf("failed to create PUT request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/octet-stream")
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("AccessKey", STORAGE_ACCESS_KEY)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to upload file %s: %v", localPath, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusCreated {
+		fmt.Printf("upload successful for: %s\n", localPath)
+	} else {
+		respBody, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed to upload %s: %d - %s", localPath, resp.StatusCode, string(respBody))
+	}
+	return nil
+}
